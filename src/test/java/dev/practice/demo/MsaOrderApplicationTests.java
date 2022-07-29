079408601d144c32ec5bf33cafad1fc1d4503b3b
SONGYOUNGWOO <jeroso@ldfactory.com>
2022-07-29 13:32:41 +0900

    MSA order 프로젝트 생성

---
+++ src/test/java/dev/practice/demo/MsaOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
